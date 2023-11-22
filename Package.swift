c4962c50b3cf0eccaa2da4e5bf7ee5159d9b9046
Graham Berks <graham@fatlazycat.com>
2023-11-22 19:23:10 +0000

    Package deps

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "adventOfCode2023",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "adventOfCode2023",
+            targets: ["adventOfCode2023"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-parsing", .upToNextMajor(from: "0.13.0")),
+        .package(url: "https://github.com/nschum/SwiftHamcrest", .upToNextMajor(from: "2.2.4")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "adventOfCode2023"),
+        .testTarget(
+            name: "adventOfCode2023Tests",
+            dependencies: ["adventOfCode2023"]),
+    ]
+)
---
+++ Tests/adventOfCode2023Tests/adventOfCode2023Tests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import adventOfCode2023
+
+final class adventOfCode2023Tests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
