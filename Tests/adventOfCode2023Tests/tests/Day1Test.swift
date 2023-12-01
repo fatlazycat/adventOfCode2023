718017116d68aeff1444144608e3f329e2d0e663
Graham Berks <graham@fatlazycat.com>
2023-12-01 11:56:17 +0000

    Finished Day 1 Part 2

--- Tests/adventOfCode2023Tests/tests/Day1Test.swift
+++ Tests/adventOfCode2023Tests/tests/Day1Test.swift
@@ -6,26 +6,61 @@ import SwiftHamcrest
 class Day1Test : XCTestCase {
     
     func testDay1Part1Dummy() {
-        let data = day1DummyData.lines
-        let pairs = data.map{ ($0[$0.firstIndex(where: {$0.isNumber})!],
-                               $0[$0.lastIndex(where: {$0.isNumber})!]) }
-        let numbers = pairs.map{ Int(String([$0.0, $0.1]))! }
-        assertThat(numbers.reduce(0, +) == 142)
+        assertThat(calc(lines: day1DummyData.lines) == 142)
     }
     
     func testDay1Part1() {
-        let data = day1Data.lines
-        let pairs = data.map{ ($0[$0.firstIndex(where: {$0.isNumber})!],
+        assertThat(calc(lines: day1Data.lines) == 54968)
+    }
+    
+    func testDay1Part2Dummy() {
+        let data = day1DummyData2.lines.map{ replaceSpelledNumbers(s: $0) }
+        assertThat(calc(lines: data) == 281)
+    }
+    
+    func testDay1Part2() {
+        let data = day1Data.lines.map{ replaceSpelledNumbers(s: $0) }
+        assertThat(calc(lines: data) == 54094)
+    }
+    
+    func calc(lines: [String]) -> Int {
+        let pairs = lines.map{ ($0[$0.firstIndex(where: {$0.isNumber})!],
                                $0[$0.lastIndex(where: {$0.isNumber})!]) }
         let numbers = pairs.map{ Int(String([$0.0, $0.1]))! }
-        assertThat(numbers.reduce(0, +) == 54968)
+        
+        return numbers.reduce(0, +)
+    }
+    
+    func replaceSpelledNumbers(s: String) -> String {
+        foldl(sequence: spelledOutNumbers, base: s, transform:{ (acc, e) in acc.replacingOccurrences(of: e.key, with: e.value) })
     }
     
 }
+
+public let spelledOutNumbers =
+["one": "o1e",
+ "two": "t2o",
+ "three": "t3e",
+ "four": "f4r",
+ "five": "f5e",
+ "six": "s6x",
+ "seven": "s7n",
+ "eight": "e8t",
+ "nine": "n9e"]
 
 public let day1DummyData = """
 1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet
+"""
+
+public let day1DummyData2 = """
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
 """
