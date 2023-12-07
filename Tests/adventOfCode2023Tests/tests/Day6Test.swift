f656e995e6eee1a6db48d4f609678ce694700e24
Graham Berks <graham@fatlazycat.com>
2023-12-07 14:49:23 -0500

    Refactor

--- Tests/adventOfCode2023Tests/tests/Day6Test.swift
+++ Tests/adventOfCode2023Tests/tests/Day6Test.swift
@@ -10,14 +10,40 @@ class Day6Test : XCTestCase {
         assertThat(results.reduce(1, *) == 288)
     }
     
+    func testPart1DummyCalc() {
+        let results = day6DummyData.map{ calcPasses(raceTime: $0.time, record: $0.record) }
+        assertThat(results.reduce(1, *) == 288)
+    }
+    
     func testPart1() {
         let results = day6Data.map{ calcDistance(raceTime: $0.time, record: $0.record) }
+        assertThat(results.reduce(1, *) == 131376)
+    }
+    
+    func testPart1Calc() {
+        let results = day6Data.map{ calcPasses(raceTime: $0.time, record: $0.record) }
         assertThat(results.reduce(1, *) == 131376)
     }
     
     func calcDistance(raceTime: Int, record: Int) -> Int {
         (0...raceTime).map{ $0 * (raceTime - $0) }.filter{ $0 > record }.count
     }
+    
+    func calcPasses(raceTime: Int, record: Int) -> Int {
+        let min = calcMin(raceTime: raceTime, record: record)
+        let max = calcMax(raceTime: raceTime, record: record)
+        
+        return max - min + 1
+    }
+    
+    func calcMin(raceTime: Int, record: Int) -> Int {
+        (0...raceTime).first{ $0 * (raceTime - $0) > record }!
+    }
+    
+    func calcMax(raceTime: Int, record: Int) -> Int {
+        (0...raceTime).reversed().first{ $0 * (raceTime - $0) > record }!
+    }
+    
 }
 
 let day6DummyData = [
@@ -29,4 +55,6 @@ let day6DummyData = [
 //Time:      7  15   30
 //Distance:  9  40  200
 
-// Hold * (Race - Hold)
+// Hold * (Race - Hold) = record
+// Record 9
+// Hold * ( 9 - hold ) = 9
