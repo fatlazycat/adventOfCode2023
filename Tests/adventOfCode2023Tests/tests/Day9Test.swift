18e54d355030840812b88c70945e6a6a7686a9fa
Graham Berks <graham@fatlazycat.com>
2023-12-09 14:10:19 +0000

    Day 9 Done

--- Tests/adventOfCode2023Tests/tests/Day9Test.swift
+++ Tests/adventOfCode2023Tests/tests/Day9Test.swift
@@ -17,6 +17,18 @@ class Day9Test : XCTestCase {
         assertThat(lastVals.reduce(0, +) == 1708206096)
     }
     
+    func testPart2Dummy(){
+        let data = try! parser.parse(day9DummyData)
+        let lastVals = data.map{ part2(data: $0) }
+        assertThat(lastVals.reduce(0, +) == 2)
+    }
+    
+    func testPart2(){
+        let data = try! parser.parse(day9Data)
+        let lastVals = data.map{ part2(data: $0) }
+        assertThat(lastVals.reduce(0, +) == 1050)
+    }
+
     func part1(data: [Int]) -> Int {
         let paired = data.windowed(size: 2)
         let diff = paired.map{ $0[1] - $0[0] }
@@ -25,6 +37,17 @@ class Day9Test : XCTestCase {
             return data.last! + diff.last!
         } else {
             return data.last! + part1(data: diff)
+        }
+    }
+    
+    func part2(data: [Int]) -> Int {
+        let paired = data.windowed(size: 2)
+        let diff = paired.map{ $0[1] - $0[0] }
+        
+        if diff.filter({$0 != 0}).count == 0 {
+            return data.first! - diff.last!
+        } else {
+            return data.first! - part2(data: diff)
         }
     }
     
