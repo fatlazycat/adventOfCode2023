2f7b7ad2a342e89d08653275e4a464b0155244dc
Graham Berks <graham@fatlazycat.com>
2023-12-13 18:05:57 +0000

    Day 13 Part 1 Done

--- Tests/adventOfCode2023Tests/tests/Day13Test.swift
+++ Tests/adventOfCode2023Tests/tests/Day13Test.swift
@@ -6,58 +6,188 @@ class Day13Test : XCTestCase {
     
     func testPart1Dummy(){
         let data = parseData(lines: day13DummyData)
-        let verticals = data.map{ findReflection(lines: rotateArray90Degrees($0)) }.compactMap{ $0 }
-        let horizontals = data.map{ findReflection(lines: $0) }.compactMap{ $0 }
+        let results = data.map{ getMirrorLineCalc(data: $0) }
         
-        let verticalSum = verticals.reduce(0, { (acc, item) in acc + item.0 })
-        let horizontalSum = horizontals.reduce(0, { (acc, item) in acc + (item.0*100) })
+        XCTAssertEqual(results.reduce(0, +), 405)
+    }
+    
+    func testPart1(){
+        let data = parseData(lines: day13Data)
+        let results = data.map{ getMirrorLineCalc(data: $0) }
         
-        XCTAssertEqual(verticalSum + horizontalSum, 405)
+        XCTAssertEqual(results.reduce(0, +), 27505)
     }
     
     func testWindow() {
-//        let data = parseData(lines: day13DummyData)
-//        let horizontal = findReflection(lines: data[1])
-//        print(horizontal)
+        let data = parseData(lines: day13DummyData)
         
-        print("hello" == "hellothere")
-    }
-
-    func findReflection(lines: [String]) -> (Int, Int)? {
-        let rowsPaired = lines.windowed(size: 4, step: 1)
-        let match = rowsPaired.enumerated().first{ (index, pair) in
-            pair[0] == pair[3] && pair[1] == pair[2]
-        }
-
-        if let result = match {
-            return (result.offset+2, result.offset+3)
-        } else {
-            return nil
-        }
-        
+        let vertical1 = reduceFromTop(rotateArray90Degrees(data[1]))
+        let vertical2 = reduceFromBottom(rotateArray90Degrees(data[1]))
+        let horizontal1 = reduceFromTop(data[1])
+        let horizontal2 = reduceFromBottom(data[1])
+        print(vertical1)
+        print("---")
+        print(vertical2)
+        print("---")
+        print(horizontal1)
+        print("---")
+        print(horizontal2)
     }
     
-    func rotateArray90Degrees(_ input: [String]) -> [String] {
-        let rowCount = input.count
-        let colCount = input[0].count
+    func testWindow2() {
+        let data = parseData(lines: day13DummyData)
         
-        var rotatedArray = [String](repeating: "", count: colCount)
+        let vertical1 = reduceFromTop(rotateArray90Degrees(data[0]))
+        let vertical2 = reduceFromBottom(rotateArray90Degrees(data[0]))
+        let horizontal1 = reduceFromTop(data[0])
+        let horizontal2 = reduceFromBottom(data[0])
+        print(vertical1)
+        print("---")
+        print(vertical2)
+        print("---")
+        print(horizontal1)
+        print("---")
+        print(horizontal2)
+    }
+    
+    func getMirrorLineCalc(data: [[Character]]) -> Int {
+        let vertical = [reduceFromTop(rotateArray90Degrees(data)) + reduceFromBottom(rotateArray90Degrees(data))].flatMap{ $0 }.map{ $0.first }
+        let horizontal = [reduceFromTop(data) + reduceFromBottom(data)].flatMap{ $0 }.map{ $0.first * 100 }
         
-        for i in 0..<colCount {
-            for j in 0..<rowCount {
-                let charIndex = input[j].index(input[j].startIndex, offsetBy: i)
-                rotatedArray[i].append(input[j][charIndex])
+        return (vertical + horizontal).reduce(0, +)
+    }
+
+    func reduceFromBottom(_ inputLines: [[Character]], index: Int = 1) -> [Match] {
+        let height = inputLines.count
+
+        if height < 2 {
+            return []
+        }
+        
+        var lines = inputLines
+        
+        // build comparisons
+        if height.isOdd() {
+            lines = inputLines.dropLast()
+        }
+
+        let pairs = pairUntilExhausted(lines)
+        
+        if pairs.filter({$0.0 == $0.1}).count == pairs.count {
+            return [Match(index + pairs.count-1, index + pairs.count, pairs.count)]
+        } else {
+            // didn't match so shrink
+            return reduceFromBottom(Array(lines.dropLast()), index: index)
+        }
+    }
+    
+    func reduceFromTop(_ inputLines: [[Character]], startIndex: Int = 1) -> [Match] {
+        let height = inputLines.count
+
+        if height < 2 {
+            return []
+        }
+        
+        var lines = inputLines
+        var index = startIndex
+        
+        // build comparisons
+        if height.isOdd() {
+            lines = inputLines.dropFirstArray()
+            index = index + 1
+        }
+
+        let pairs = pairUntilExhausted(lines)
+        
+        if pairs.filter({$0.0 == $0.1}).count == pairs.count {
+            return [Match(index + pairs.count-1, index + pairs.count, pairs.count)]
+        } else {
+            // didn't match so shrink
+            return reduceFromTop(Array(lines.dropFirstArray()), startIndex: index+1)
+        }
+    }
+    
+    
+    func findReflection(lines: [[Character]], index: Int = 1) -> [Match] {
+        let width = lines.count
+
+        if width < 2 {
+            return []
+        }
+        
+        // build comparisons
+        if width.isOdd() {
+            let startPos = findReflection(lines: Array(lines.dropLast()), index: index)
+            let endPos = findReflection(lines: Array(lines.dropFirst()), index: index+1)
+            
+            return [startPos + endPos].flatMap{$0}
+        } else {
+            let pairs = pairUntilExhausted(lines)
+            
+            if pairs.filter({$0.0 == $0.1}).count == pairs.count {
+                return [Match(index + pairs.count-1, index + pairs.count, pairs.count)]
+            } else {
+                // didn't match so shrink
+                let startPos = findReflection(lines: Array(lines.dropLast()), index: index)
+                let endPos = findReflection(lines: Array(lines.dropFirst()), index: index+1)
+                
+                return [startPos + endPos].flatMap{$0}
+            }
+        }
+    }
+    
+    struct Match {
+        let first: Int
+        let second: Int
+        let size: Int
+        
+        init(_ first: Int, _ second: Int, _ size: Int) {
+            self.first = first
+            self.second = second
+            self.size = size
+        }
+    }
+    
+    func pairUntilExhausted(_ inputArray: [[Character]]) -> [([Character], [Character])] {
+        var result: [([Character], [Character])] = []
+        var array = inputArray
+        
+        while array.count >= 2 {
+            let firstElement = array.removeFirst()
+            let lastElement = array.removeLast()
+            result.append((firstElement, lastElement))
+        }
+        
+        return result
+    }
+    
+    func rotateArray90Degrees(_ inputArray: [[Character]]) -> [[Character]] {
+        let numRows = inputArray.count
+        let numCols = inputArray[0].count
+        
+        var rotatedArray = Array(repeating: Array(repeating: Character(" "), count: numRows), count: numCols)
+        
+        for i in 0..<numRows {
+            for j in 0..<numCols {
+                rotatedArray[j][numRows - 1 - i] = inputArray[i][j]
             }
         }
         
         return rotatedArray
     }
     
-    func parseData(lines: [String]) -> [[String]] {
+    func printCharacterGrid(_ grid: [[Character]]) {
+        for row in grid {
+            let rowString = String(row)
+            print(rowString)
+        }
+    }
+    
+    func parseData(lines: [String]) -> [[[Character]]] {
         lines.reduce([[]]) { (result: [[String]], line: String) -> [[String]] in
             guard let last = result.last else { return [[line]] }
             return line.isEmpty ? result + [[]] : result.dropLast() + [last + [line]]
-        }.filter { !$0.isEmpty }
+        }.filter { !$0.isEmpty }.map{ arr in arr.map{ s in s.toCharArray()} }
     }
     
 //    let parser = Parse(input: Substring.self) {
--- Tests/adventOfCode2023Tests/tests/DummyTest.swift
+++ Tests/adventOfCode2023Tests/tests/DummyTest.swift
@@ -17,5 +17,37 @@ class DummyTest: XCTestCase {
         print(data)
         assertThat(data == [12,32])
     }
+    
+    func testGPT() {
+        func rotateArray90Degrees(_ inputArray: [[Character]]) -> [[Character]] {
+            let numRows = inputArray.count
+            let numCols = inputArray[0].count
+            
+            var rotatedArray = Array(repeating: Array(repeating: Character(" "), count: numRows), count: numCols)
+            
+            for i in 0..<numRows {
+                for j in 0..<numCols {
+                    rotatedArray[j][numRows - 1 - i] = inputArray[i][j]
+                }
+            }
+            
+            return rotatedArray
+        }
 
+        // Example input array
+        let inputArray: [[Character]] = [
+            ["A", "B", "C"],
+            ["D", "E", "F"],
+            ["G", "H", "I"]
+        ]
+
+        let rotatedArray = rotateArray90Degrees(inputArray)
+
+        for row in rotatedArray {
+            let rowString = String(row)
+            print(rowString)
+        }
+    }
 }
+
+
