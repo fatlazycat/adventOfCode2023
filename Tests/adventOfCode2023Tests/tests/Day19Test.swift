58291e9b57c89d4744515c7d39ff4cd4028c3c87
Graham Berks <graham@fatlazycat.com>
2023-12-19 14:55:01 +0000

    Tidy up

--- Tests/adventOfCode2023Tests/tests/Day19Test.swift
+++ Tests/adventOfCode2023Tests/tests/Day19Test.swift
@@ -6,30 +6,30 @@ class Day19Test : XCTestCase {
     
     func testPart1Dummy(){
         let rawData = parseData(lines: day19DummyData.lines)
-        let workflows = rawData[0].map{ try! Day19Test.workflowParser.parse($0) }.map{ Workflow.init(name: $0.0, rules: $0.1) }
-        let ratings = rawData[1].map{ try! Day19Test.ratingParser.parse($0) }.map{ Rating(x: $0.0, m: $0.1, a: $0.2, s: $0.3) }
+        let workflows = rawData[0].map{ try! Day19Test.workflowParser.parse($0) }
+        let ratings = rawData[1].map{ try! Day19Test.ratingParser.parse($0) }
         let workflowMap = Dictionary(uniqueKeysWithValues: workflows.map{ ($0.name, $0) })
         XCTAssertEqual(19114, part1(ratings: ratings, workflowMap: workflowMap))
     }
     
     func testPart1(){
         let rawData = parseData(lines: day19Data.lines)
-        let workflows = rawData[0].map{ try! Day19Test.workflowParser.parse($0) }.map{ Workflow.init(name: $0.0, rules: $0.1) }
-        let ratings = rawData[1].map{ try! Day19Test.ratingParser.parse($0) }.map{ Rating(x: $0.0, m: $0.1, a: $0.2, s: $0.3) }
+        let workflows = rawData[0].map{ try! Day19Test.workflowParser.parse($0) }
+        let ratings = rawData[1].map{ try! Day19Test.ratingParser.parse($0) }
         let workflowMap = Dictionary(uniqueKeysWithValues: workflows.map{ ($0.name, $0) })
         XCTAssertEqual(374873, part1(ratings: ratings, workflowMap: workflowMap))
     }
     
     func testPart2Dummy(){
         let rawData = parseData(lines: day19DummyData.lines)
-        let workflows = rawData[0].map{ try! Day19Test.workflowParser.parse($0) }.map{ Workflow.init(name: $0.0, rules: $0.1) }
+        let workflows = rawData[0].map{ try! Day19Test.workflowParser.parse($0) }
         let workflowMap = Dictionary(uniqueKeysWithValues: workflows.map{ ($0.name, $0) })
         XCTAssertEqual(167409079868000, part2(workflowMap: workflowMap))
     }
     
     func testPart2(){
         let rawData = parseData(lines: day19Data.lines)
-        let workflows = rawData[0].map{ try! Day19Test.workflowParser.parse($0) }.map{ Workflow.init(name: $0.0, rules: $0.1) }
+        let workflows = rawData[0].map{ try! Day19Test.workflowParser.parse($0) }
         let workflowMap = Dictionary(uniqueKeysWithValues: workflows.map{ ($0.name, $0) })
         XCTAssertEqual(122112157518711, part2(workflowMap: workflowMap))
     }
@@ -223,19 +223,19 @@ class Day19Test : XCTestCase {
     }
     
     func testParsers() {
-        XCTAssertEqual([Workflow(name: "in", rules: [Rule.Reject])], [try! Day19Test.workflowParser.parse("in{R}")].map{ Workflow.init(name: $0.0, rules: $0.1) })
+        XCTAssertEqual([Workflow(name: "in", rules: [Rule.Reject])], [try! Day19Test.workflowParser.parse("in{R}")])
         XCTAssertEqual(Rule.Condition("b", "<", 1, "qz"), try! Day19Test.ruleParser.parse("b<1:qz"))
-        XCTAssertEqual([Workflow(name: "in", rules: [Rule.Condition("b", "<", 1, "qz")])], [try! Day19Test.workflowParser.parse("in{b<1:qz}")].map{ Workflow.init(name: $0.0, rules: $0.1) })
+        XCTAssertEqual([Workflow(name: "in", rules: [Rule.Condition("b", "<", 1, "qz")])], [try! Day19Test.workflowParser.parse("in{b<1:qz}")])
         
         XCTAssertEqual([Workflow(name: "in", rules: [Rule.Condition("b", "<", 1, "qz"),Rule.Condition("c", ">", 2, "qq"), Rule.Accept])],
-                       [try! Day19Test.workflowParser.parse("in{b<1:qz,c>2:qq,A}")].map{ Workflow.init(name: $0.0, rules: $0.1) })
+                       [try! Day19Test.workflowParser.parse("in{b<1:qz,c>2:qq,A}")])
         
         XCTAssertEqual([Workflow(name: "in", rules: [Rule.Condition("a", "<", 2006, "qkq"), Rule.Condition("m", ">", 2090, "A"), Rule.Destination("rfg")])],
-                       [try! Day19Test.workflowParser.parse("in{a<2006:qkq,m>2090:A,rfg}")].map{ Workflow.init(name: $0.0, rules: $0.1) })
+                       [try! Day19Test.workflowParser.parse("in{a<2006:qkq,m>2090:A,rfg}")])
     }
     
     func testRatingParser() {
-        XCTAssertEqual([Rating(x: 787, m: 2655, a: 1222, s: 2876)], [try! Day19Test.ratingParser.parse("{x=787,m=2655,a=1222,s=2876}")].map{ Rating(x: $0.0, m: $0.1, a: $0.2, s: $0.3) })
+        XCTAssertEqual([Rating(x: 787, m: 2655, a: 1222, s: 2876)], [try! Day19Test.ratingParser.parse("{x=787,m=2655,a=1222,s=2876}")])
     }
     
     static let ratingParser = Parse(input: Substring.self) {
@@ -248,7 +248,7 @@ class Day19Test : XCTestCase {
         ",s="
         Int.parser()
         "}"
-    }
+    }.map{ Rating(x: $0.0, m: $0.1, a: $0.2, s: $0.3) }
     
     static let workflowParser = Parse(input: Substring.self) {
         Prefix{ $0 != "{" }
@@ -259,7 +259,7 @@ class Day19Test : XCTestCase {
             ","
         }
         Skip { "}" }
-    }
+    }.map{ Workflow.init(name: $0.0, rules: $0.1) }
     
     static let ruleParser = Parse(input: Substring.UTF8View.self) {
         OneOf {
