b18a82b2eb95a9fa815938fd75f3accb6ec52746
Graham Berks <graham@fatlazycat.com>
2023-12-20 13:32:40 +0000

    WIP

---
+++ Tests/adventOfCode2023Tests/tests/Day20Test.swift
@@ -0,0 +1,219 @@
+import XCTest
+import Parsing
+import Foundation
+
+class Day20Test : XCTestCase {
+    
+    func testPart1Dummy(){
+        let data = try! Day20Test.parser.parse(day20DummyData)
+        let graph = constructGraph(data: data)
+        let result = processPulse(instruction: Instruction(to: "broadcaster", from: "button", pulse: .Low), startingModules: graph)
+        
+        XCTAssertEqual(32000000, result.0 * result.1 * 1000 * 1000)
+    }
+    
+    func processPulse(instruction: Instruction, startingModules: [String : Module]) -> (Int, Int) {
+        var modules = startingModules
+        var instructions = [instruction]
+        var lowCount = 1
+        var highCount = 0
+        
+//        print("===")
+        
+        while !instructions.isEmpty {
+            let i = instructions.removeFirst()
+            let m = modules[i.to]!
+            var pulseType = Pulse.Low
+            var pulseCount = 0
+            
+//            print(i.AsString() + "\n")
+            
+            switch m {
+            case .Standard(name: let name, outputs: let outputs):
+                let newInstructions = outputs.map{ Instruction(to: $0, from: name, pulse: i.pulse) }
+                instructions.append(contentsOf: newInstructions)
+                pulseType = i.pulse
+                pulseCount += newInstructions.count
+            case .FlipFlop(name: let name, outputs: let outputs, state: let state):
+                switch i.pulse {
+                case .High:
+                    break
+                case .Low:
+                    switch state {
+                    case .On:
+                        modules[name] = Module.FlipFlop(name: name, outputs: outputs, state: .Off)
+                        let newInstructions = outputs.map{ Instruction(to: $0, from: name, pulse: .Low) }
+                        instructions.append(contentsOf: newInstructions)
+                        pulseType = .Low
+                        pulseCount += newInstructions.count
+                    case .Off:
+                        modules[name] = Module.FlipFlop(name: name, outputs: outputs, state: .On)
+                        let newInstructions = outputs.map{ Instruction(to: $0, from: name, pulse: .High) }
+                        instructions.append(contentsOf: newInstructions)
+                        pulseType = .High
+                        pulseCount += newInstructions.count
+                    }
+                }
+            case .Conjunction(name: let name, inputs: let inputs, outputs: let outputs):
+                var updatedInputs = inputs
+                updatedInputs[i.from] = i.pulse
+                modules[name] = Module.Conjunction(name: name, inputs: updatedInputs, outputs: outputs)
+                
+                if updatedInputs.allSatisfy({ $0.value == .High}) {
+                    let newInstructions = outputs.map{ Instruction(to: $0, from: name, pulse: .Low) }
+                    instructions.append(contentsOf: newInstructions)
+                    pulseType = .Low
+                    pulseCount += newInstructions.count
+                } else {
+                    let newInstructions = outputs.map{ Instruction(to: $0, from: name, pulse: .High) }
+                    instructions.append(contentsOf: newInstructions)
+                    pulseType = .High
+                    pulseCount += newInstructions.count
+                }
+            }
+            
+            switch pulseType {
+            case .High:
+                highCount += pulseCount
+            case .Low:
+                lowCount += pulseCount
+            }
+        }
+        
+        return (lowCount, highCount)
+    }
+    
+    func constructGraph(data: [(Substring, [Substring])]) -> [String : Module] {
+        var modules = [String : Module]()
+        
+        // Create the modules
+        data.forEach({ m in
+            switch m.0.first! {
+            case "%":
+                let name = String(m.0.dropFirst())
+                modules[name] = Module.FlipFlop(name: name, outputs: [], state: .Off)
+            case "&":
+                let name = String(m.0.dropFirst())
+                modules[name] = Module.Conjunction(name: name, inputs: [:], outputs: [])
+            default:
+                let name = String(m.0)
+                modules[name] = Module.Standard(name: name, outputs: [])
+            }
+        })
+        
+        // Map the outputs
+        data.forEach({ m in
+            switch m.0.first! {
+            case "%":
+                let name = String(m.0.dropFirst())
+                let outputs = m.1.map{ modules[String($0)]!.moduleName }
+                modules[name] = Module.FlipFlop(name: name, outputs: outputs, state: .Off)
+            case "&":
+                let name = String(m.0.dropFirst())
+                let outputs = m.1.map{ modules[String($0)]!.moduleName }
+                modules[name] = Module.Conjunction(name: name, inputs: [:], outputs: outputs)
+            default:
+                let name = String(m.0)
+                let outputs = m.1.map{ modules[String($0)]!.moduleName }
+                modules[name] = Module.Standard(name: name, outputs: outputs)
+            }
+        })
+        
+        // Map the inputs for Conjunctions
+        modules.values.forEach({ m in
+            switch m {
+            case .Standard(name: _, outputs: _):
+                break
+            case .FlipFlop(name: _, outputs: _, state: _):
+                break
+            case .Conjunction(name: let name, inputs: _, outputs: let outputs):
+                let newInputs = Dictionary(uniqueKeysWithValues: modules.values.filter({$0.moduleOutputs.contains(m.moduleName)}).map{($0.moduleName, Pulse.Low)})
+                modules[name] = Module.Conjunction(name: name, inputs: newInputs, outputs: outputs)
+            }
+        })
+        
+        modules["button"] = Module.Standard(name: "button", outputs: ["broadcaster"])
+        
+        return modules
+    }
+    
+    func isConjunction(_ m: Module) -> Bool {
+        if case Module.Conjunction(_, _, _) = m {
+            true
+        } else {
+            false
+        }
+    }
+    
+    struct Instruction: Equatable, Hashable {
+        let to: String
+        let from: String
+        let pulse: Pulse
+        
+        func AsString() -> String {
+            "from: \(from) to: \(to) type \(pulse.asString)"
+        }
+    }
+    
+    enum Module: Equatable, Hashable {
+        case Standard(name: String, outputs: [String])
+        case FlipFlop(name: String, outputs: [String], state: State)
+        case Conjunction(name: String, inputs: [String : Pulse], outputs: [String])
+        
+        var moduleName: String {
+            switch self {
+            case .Standard(name: let name, _):
+                return name
+            case .FlipFlop(name: let name, _, _):
+                return name
+            case .Conjunction(name: let name, _, _):
+                return name
+            }
+        }
+        
+        var moduleOutputs: [String] {
+            switch self {
+            case .Standard(_, outputs: let outputs):
+                return outputs
+            case .FlipFlop(_, outputs: let outputs, _):
+                return outputs
+            case .Conjunction(_, _, outputs: let outputs):
+                return outputs
+            }
+        }
+    }
+    
+    enum State: Equatable, Hashable {
+        case On
+        case Off
+    }
+    
+    enum Pulse : Equatable, Hashable {
+        case High
+        case Low
+        
+        var asString: String {
+            switch self {
+            case .High:
+                "High"
+            case .Low:
+                "Low"
+            }
+        }
+    }
+    
+    static let parser = Parse(input: Substring.self) {
+        Many {
+            Prefix{ $0 != " " }
+            Skip{ " -> "}
+            Many {
+                Prefix{ $0 != "," && $0 != "\n" }
+            } separator: {
+                ", "
+            }
+        } separator: {
+            Whitespace(1, .vertical)
+        }
+    }
+    
+}
